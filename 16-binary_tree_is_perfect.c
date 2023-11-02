65fa8e8995beffd636cdde92e889b1583145b693
Addy <123188947+addymwenda12@users.noreply.github.com>
2023-11-02 23:30:48 +0300

    function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,6 +18,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: A pointer to the root node of the tree
+ *
+ * Return: The number of leaves in the tree, or 0 if `tree` is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: A pointer to the root node of the tree
  *
@@ -33,5 +51,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     height = binary_tree_height(tree);
     leaves = binary_tree_leaves(tree);
 
-    return (leaves == (1 << height));
+    if (leaves == (size_t)(1 << height))
+		return (1);
+
+	return (0);
 }
